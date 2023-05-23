a8c31975e5e97b186f5954fd52b9cfdfc9205e28
Muniba Ahmed <mahmed93@berkeley.edu>
2023-05-23 11:39:49 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # donutshop
+
+cd into donutshop1
---
+++ donutshop1/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
+
+//front-end
+//pages: home, location(gps?), donuts
+//components: navbar, footer, donutitem?
+//helper: donutlist?
+//assets: all donut pictures 10? same size
+//styles css?
+//contact page with form.
+
+//back-end?
