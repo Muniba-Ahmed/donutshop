f8ad88050e01b50343d89994c54cf6f7aa6e2d0f
Muniba Ahmed <mahmed93@berkeley.edu>
2023-05-23 12:26:21 -0700

    working on navbar

---
+++ donutshop1/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      Navbar
+      <div className="toggleButton">
+        <button></button>
+      </div>
+      <div className="links"></div>
+    </div>
+  );
+}
+
+export default Navbar;
