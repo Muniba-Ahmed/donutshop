c539166661f54fb7908e9c7af0e1dc677c0dfc13
Muniba Ahmed <mahmed93@berkeley.edu>
2023-05-23 17:13:41 -0700

    navbar rendering

--- donutshop1/src/components/Navbar.js
+++ donutshop1/src/components/Navbar.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { slide as Menu } from "react-burger-menu";
+import { Link, useLocation } from "react-router-dom";
+// import ReorderIcon from "@material-ui/icons/Reorder";
+import "../styles/Navbar.css";
 
 function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+
+  const toggleMenu = () => {
+    setExpandNavbar(!expandNavbar);
+  };
+  const stateChangeHandler = (newState) => {
+    setExpandNavbar(newState.isOpen);
+  };
+
   return (
-    <div className="navbar">
-      Navbar
-      <div className="toggleButton">
-        <button></button>
-      </div>
-      <div className="links"></div>
+    <div className="toggleButton">
+      <button onClick={toggleMenu}> Toggle Menu</button>
+
+      <Menu
+        customBurgerIcon={false}
+        isOpen={expandNavbar}
+        onStateChange={stateChangeHandler}
+      >
+        <Link to="/" className="bm-item">
+          Home
+        </Link>
+        <Link to="/donuts" className="bm-item">
+          Donuts
+        </Link>
+        <Link to="/about" className="bm-item">
+          About
+        </Link>
+        <Link to="/location" className="bm-item">
+          Location
+        </Link>
+        <Link to="/contactform" className="bm-item">
+          Contact Us
+        </Link>
+        <a onClick={toggleMenu} className="menu-item--small" href="a">
+          Settings
+        </a>
+      </Menu>
     </div>
   );
 }
